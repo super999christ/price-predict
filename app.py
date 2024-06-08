03c2d833c6ad5c741acd11c8362a9901cc0ac677
Cody Christ <cchrist1210@gmail.com>
2024-06-08 11:06:18 -0700

    Test price prediction and connect with API endpoints

--- app.py
+++ app.py
@@ -14,3 +14,29 @@ app = FastAPI()
 def read_root():
     return {"response": "Ready!"}
 
+
+@app.exception_handler(RequestValidationError)
+async def validation_exception_handler(request, exc):
+    key = exc.errors()[0]['loc'][1]
+    return JSONResponse(
+        status_code=status.HTTP_422_UNPROCESSABLE_ENTITY,
+        content={"error": f"{key} {exc.errors()[0]['msg']}"},
+    )
+
+
+@app.post("/predict")
+def prediction(prop: Property):
+    try:
+        dict_prop = jsonable_encoder(prop)
+        dict_prop = pd.DataFrame([dict_prop])
+        y_pred = model.predict(dict_prop)
+        price = float(y_pred[0])
+        return {
+            "prediction": price,
+            "status_code": status.HTTP_200_OK
+        }
+    except RequestValidationError as e:
+        return JSONResponse(
+            status_code=status.HTTP_422_UNPROCESSABLE_ENTITY,
+            content=e,
+        )
